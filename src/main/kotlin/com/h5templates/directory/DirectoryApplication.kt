ccaf9b838113b49aeba2931a02d38559d4f45b2c
dainiusstep <dstepulevicius@fcptech.com>
2023-08-06 14:07:37 +0100

    add project files

---
+++ src/main/kotlin/com/h5templates/directory/DirectoryApplication.kt
@@ -0,0 +1,11 @@
+package com.h5templates.directory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DirectoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DirectoryApplication>(*args)
+}
