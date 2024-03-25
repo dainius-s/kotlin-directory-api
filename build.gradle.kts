bc405c899540b879ddbffa424df86be31ffa2a83
dainiusstep <dstepulevicius@fcptech.com>
2024-03-25 21:39:32 +0000

    move JWT key generate into the gradle taks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import java.util.Base64
 
 plugins {
 	id("org.springframework.boot") version "3.1.2"
@@ -24,12 +27,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("com.h2database:h2")
@@ -37,6 +39,7 @@ dependencies {
 	testImplementation("io.mockk:mockk:1.10.4")
 //	testImplementation("io.mockk:mockk-spring:1.12.0")
 	testImplementation("com.ninja-squad:springmockk:3.0.1")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
@@ -48,4 +51,23 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+	dependencies {
+		classpath("io.jsonwebtoken:jjwt-api:0.11.2")
+		classpath("io.jsonwebtoken:jjwt-impl:0.11.2")
+		classpath("io.jsonwebtoken:jjwt-jackson:0.11.2")
+	}
+}
+
+tasks.register("generateJwtKey") {
+	doLast {
+		val key = Keys.secretKeyFor(SignatureAlgorithm.HS256) // Generate the key
+		val encodedKey = Base64.getEncoder().encodeToString(key.encoded) // Encode key as string
+		println("Secure key for HS256: $encodedKey")
+	}
 }
